ad968bba3a71842cec690400ed589e7efe965f82
Icaro Oliveira <icarovinici@gmail.com>
2021-09-28 01:02:32 -0300

    chore: config app module for db and .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path'
+
+import { StudentModule } from './student/student.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, //only in development
+    }),
+    StudentModule,
+  ],
   controllers: [],
   providers: [],
 })
