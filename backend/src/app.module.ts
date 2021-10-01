346a5968a6be07f3fc52689575c5deb5b66c3a12
Icaro Oliveira <icarovinici@gmail.com>
2021-09-30 22:36:58 -0300

    refactor: enable graphql playground for prod mode

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config'
 import { join } from 'path'
 
 import { StudentModule } from './student/student.module'
@@ -11,13 +11,15 @@ import { StudentModule } from './student/student.module'
     ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      introspection: true,
+      playground: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      url: process.env.DATABASE_URL || 'postgres://user:password@postgres:5432/db',
+      username: process.env.POSTGRES_USER || 'user',
+      password: process.env.POSTGRES_PASSWORD || 'password',
+      database: process.env.POSTGRES_DB || 'db',
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true, //only in development
     }),
